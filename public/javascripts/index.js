63e260aaa1f437ff686b70dc5482c5b28b25e5c4
Rama Cerratto <ramacerratto@gmail.com>
2023-08-13 03:37:21 -0300

    creación estructura

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,21 +1,5 @@
-const logo = document.querySelector(".logo_intro");
-const zoomElement = document.querySelector(".zoom");
+import { ingestGraphData , aimNodeFromId , takeScreenshot, activateZoomToFit, activateOrbit, resetOrbit, stopOrbit} from "./landing/indexNetwork.js";
 
-let zoom = 1;
-var ZOOM_SPEED = 0.3;
-
-document.addEventListener("wheel", function(e) {  
-    if(zoomElement.clientWidth > 27000){
-        if(e.deltaY > 0){    
-            if(zoomElement.clientWidth > 1000){
-                ZOOM_SPEED = 1;
-            }
-            zoomElement.style.transform = `scale(${zoom += ZOOM_SPEED})`;  
-        }else{    
-            zoomElement.style.transform = `scale(${zoom -= ZOOM_SPEED})`;  }
-    }else{ //todo negro
-        
-    }
+jQuery(function(){
 
 });
-
