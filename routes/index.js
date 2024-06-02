605c82393f3c7ce8738cef1038bd7a5f73a6d291
Rama Cerratto <ramacerratto@gmail.com>
2024-06-02 18:01:09 -0300

    fix espora

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tropical Poncho - Collective Conscience' });
 });
 
+router.get('/espora', function(req, res, next) {
+  res.render('index', { title: 'Tropical Poncho - Collective Conscience' });
+});
+
+
 module.exports = router;
